57bd00eb9be421a6234b1b2d281a4f32a6a5cca9
RennickLad <ianrennickspain@gmail.com>
2026-07-16 21:24:17 +0100

    action cable migration fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_11_113941) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_16_202243) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -152,6 +152,14 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_11_113941) do
     t.boolean "is_private", default: false
     t.string "name"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "solid_cable_messages", force: :cascade do |t|
+    t.binary "channel", null: false
+    t.datetime "created_at", null: false
+    t.binary "payload", null: false
+    t.index ["channel"], name: "index_solid_cable_messages_on_channel"
+    t.index ["created_at"], name: "index_solid_cable_messages_on_created_at"
   end
 
   create_table "tags", force: :cascade do |t|
